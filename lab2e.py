2fa6590bd3ae5b356c39388cc0a36e34683727d2
Vidhi Patel <vmpatel41@myseneca.ca>
2026-05-15 09:23:35 -0400

    Complete lab 2 submission

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Vidhiben Maheshbhai Patel
+# Author ID: vmpatel41 124656240
+# Date Created: 2026/05/15
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
